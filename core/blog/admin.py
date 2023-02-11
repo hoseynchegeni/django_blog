389250cc6cfd8db2fc36163d2697a5f0a70fdc7c
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-11 23:42:17 +0330

    edit admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Post, Category, Comment
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'status', 'created_date')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_date')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Comment, CommentAdmin)
--- core/blog/models.py
+++ core/blog/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
     updated_date = models.DateTimeField(auto_now= True)
     published_date = models.DateTimeField()
 
+    def __str__(self):
+        return self.title
+
 
 class Category(models.Model):
     name = models.CharField(max_length= 255)
