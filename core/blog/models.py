176e340243f34ae542150663d2085e03e3b49b0a
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-11 16:52:39 +0330

    add blog app and build post category comment model

---
+++ core/blog/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
+    image = models.ImageField(null= True, blank= True)
+    title = models.CharField(max_length= 255)
+    content = models.TextField()
+    status  = models.BooleanField()
+    category = models.ForeignKey('Category', on_delete= models.SET_NULL, null= True)
+    comments = models.ForeignKey('Comments', on_delete= models.SET_NULL, null= True),
+    created_date = models.DateTimeField(auto_now_add= True)
+    updated_date = models.DateTimeField(auto_now= True)
+    published_date = models.DateTimeField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length= 255)
+
+class Comment(models.Model):
+    author = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete= models.CASCADE)
+    text = models.CharField(max_length= 500)
+    created_date = models.DateTimeField(auto_now_add= True)
+
