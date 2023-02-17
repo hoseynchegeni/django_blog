124324928bb51034d91214f48757f08dc18b9580
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-18 03:14:37 +0330

    add is-author-or-read-only permission and use it in post detail

--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -3,9 +3,11 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import PostListSerializer, CategorySerializer
 from ...models import Post, Category
 from rest_framework.response import Response 
+from .permissions import IsAuthorOrReadOnly
 
 
 class PostApiView(ModelViewSet):
+    permission_classes = [IsAuthorOrReadOnly]
     serializer_class = PostListSerializer
     queryset = Post.objects.filter(status = True)
 
