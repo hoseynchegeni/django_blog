f26dc64310c931fd62fb566b35dc063e72a99610
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-18 12:14:24 +0330

    add pagination to post list

--- core/blog/api/v1/pagination.py
+++ core/blog/api/v1/pagination.py
@@ -1,5 +1,11 @@
 from rest_framework.pagination import PageNumberPagination
 
+
+class PostPagination(PageNumberPagination):
+    page_size = 8
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
 class PopularPostsPagination(PageNumberPagination):
     page_size = 4
     page_size_query_param = "page_size"
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -2,17 +2,16 @@ from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from .serializers import PostSerializer, CategorySerializer
 from ...models import Post, Category
-from rest_framework.response import Response 
 from .permissions import IsAuthorOrReadOnly
 from django.db.models import Count
-from .pagination import PopularPostsPagination
+from .pagination import PopularPostsPagination, PostPagination
 
 
 class PostApiView(ModelViewSet):
     permission_classes = [IsAuthorOrReadOnly]
     serializer_class = PostSerializer
     queryset = Post.objects.filter(status = True)
-
+    pagination_class = PostPagination
 
 class CategoryApiView(ModelViewSet):
     serializer_class = CategorySerializer
