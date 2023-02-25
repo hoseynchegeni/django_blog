e2d889a3fb01f59ce9cf68498fb286cf6b1cc7dd
hoesyn <87471826+hoseynchegeni@users.noreply.github.com>
2023-02-25 15:32:11 +0330

    add tests t api

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import  PostApiView, CategoryApiView, PopularPostsAPi
 from rest_framework.routers import DefaultRouter
 
+app_name = 'api_v1'
 
 router = DefaultRouter()
 
--- core/blog/tests.py
+++ core/blog/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse 
+from rest_framework  import status
+from .models import Category
 
 # Create your tests here.
+class APITests(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.category = Category.objects.create(
+            name = 'test',
+            url = '#'
+        )
+    
+    def test_api_category_list(self):
+        response = self.client.get(reverse('blog:api_v1:category'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Category.objects.count(), 1)
+        self.assertContains(response, self.category)
